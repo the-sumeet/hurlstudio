fab320ce4456afec4e82fbdfe42d812d04f888dd
sumeet <Sumeet.Mathpati@steel-eye.com>
2025-11-20 00:19:10 +0530

    refactor sidebar icons

--- frontend/src/lib/components/app-sidebar.svelte
+++ frontend/src/lib/components/app-sidebar.svelte
@@ -1,189 +1,26 @@
 <script lang="ts">
-	import ArchiveXIcon from '@lucide/svelte/icons/archive-x';
-	import FileIcon from '@lucide/svelte/icons/file';
-	import InboxIcon from '@lucide/svelte/icons/inbox';
-	import SendIcon from '@lucide/svelte/icons/send';
 	import SunIcon from '@lucide/svelte/icons/sun';
 	import MoonIcon from '@lucide/svelte/icons/moon';
-	import AudioWaveformIcon from '@lucide/svelte/icons/audio-waveform';
 	import ArrowLeftRightIcon from '@lucide/svelte/icons/arrow-left-right';
-	import * as InputGroup from "$lib/components/ui/input-group/index.js";
-	import * as ButtonGroup from "$lib/components/ui/button-group/index.js";
+	import Settings from '@lucide/svelte/icons/settings';
 	import Wand from '@lucide/svelte/icons/wand';
 	import FilePlus from '@lucide/svelte/icons/file-plus';
 	import FolderPlus from '@lucide/svelte/icons/folder-plus';
-	import ChartPieIcon from '@lucide/svelte/icons/chart-pie';
-	import CommandIcon from '@lucide/svelte/icons/command';
-	import FrameIcon from '@lucide/svelte/icons/frame';
-	  import { Input } from "$lib/components/ui/input/index.js";
-	import GalleryVerticalEndIcon from '@lucide/svelte/icons/gallery-vertical-end';
-	import MapIcon from '@lucide/svelte/icons/map';
+	import * as InputGroup from '$lib/components/ui/input-group/index.js';
+	import * as ButtonGroup from '$lib/components/ui/button-group/index.js';
+	import { Input } from '$lib/components/ui/input/index.js';
+	import { Button } from './ui/button';
 	import { useSidebar } from '$lib/components/ui/sidebar/context.svelte.js';
 	import * as Sidebar from '$lib/components/ui/sidebar/index.js';
 	import type { ComponentProps } from 'svelte';
 	import NavFiles from './nav-files.svelte';
 	import { themeStore } from '$lib/stores/themeStore.svelte';
-	import Settings from '@lucide/svelte/icons/settings';
 	import { page } from '$app/stores';
-	import { Button } from './ui/button';
 	import { CreateFile, CreateDir, OpenFile, GetFileContent, GetCurrentFilesState } from '$lib/wailsjs/go/main/App';
 	import { fileStore } from '$lib/stores/fileStore.svelte';
-	// This is sample data
-	const data = {
-		teams: [
-			{
-				name: 'Acme Inc',
-				logo: GalleryVerticalEndIcon,
-				plan: 'Enterprise'
-			},
-			{
-				name: 'Acme Corp.',
-				logo: AudioWaveformIcon,
-				plan: 'Startup'
-			},
-			{
-				name: 'Evil Corp.',
-				logo: CommandIcon,
-				plan: 'Free'
-			}
-		],
-		projects: [
-			{
-				name: 'Design Engineering',
-				url: '#',
-				icon: FrameIcon
-			},
-			{
-				name: 'Sales & Marketing',
-				url: '#',
-				icon: ChartPieIcon
-			},
-			{
-				name: 'Travel',
-				url: '#',
-				icon: MapIcon
-			}
-		],
-		user: {
-			name: 'shadcn',
-			email: 'm@example.com',
-			avatar: '/avatars/shadcn.jpg'
-		},
-		navMain: [
-			{
-				title: 'Inbox',
-				url: '#',
-				icon: InboxIcon,
-				isActive: true
-			},
-			{
-				title: 'Drafts',
-				url: '#',
-				icon: FileIcon,
-				isActive: false
-			},
-			{
-				title: 'Sent',
-				url: '#',
-				icon: SendIcon,
-				isActive: false
-			},
-			{
-				title: 'Junk',
-				url: '#',
-				icon: ArchiveXIcon,
-				isActive: false
-			}
-		],
-		mails: [
-			{
-				name: 'William Smith',
-				email: 'williamsmith@example.com',
-				subject: 'Meeting Tomorrow',
-				date: '09:34 AM',
-				teaser:
-					'Hi team, just a reminder about our meeting tomorrow at 10 AM.\nPlease come prepared with your project updates.'
-			},
-			{
-				name: 'Alice Smith',
-				email: 'alicesmith@example.com',
-				subject: 'Re: Project Update',
-				date: 'Yesterday',
-				teaser:
-					"Thanks for the update. The progress looks great so far.\nLet's schedule a call to discuss the next steps."
-			},
-			{
-				name: 'Bob Johnson',
-				email: 'bobjohnson@example.com',
-				subject: 'Weekend Plans',
-				date: '2 days ago',
-				teaser:
-					"Hey everyone! I'm thinking of organizing a team outing this weekend.\nWould you be interested in a hiking trip or a beach day?"
-			},
-			{
-				name: 'Emily Davis',
-				email: 'emilydavis@example.com',
-				subject: 'Re: Question about Budget',
-				date: '2 days ago',
-				teaser:
-					"I've reviewed the budget numbers you sent over.\nCan we set up a quick call to discuss some potential adjustments?"
-			},
-			{
-				name: 'Michael Wilson',
-				email: 'michaelwilson@example.com',
-				subject: 'Important Announcement',
-				date: '1 week ago',
-				teaser:
-					"Please join us for an all-hands meeting this Friday at 3 PM.\nWe have some exciting news to share about the company's future."
-			},
-			{
-				name: 'Sarah Brown',
-				email: 'sarahbrown@example.com',
-				subject: 'Re: Feedback on Proposal',
-				date: '1 week ago',
-				teaser:
-					"Thank you for sending over the proposal. I've reviewed it and have some thoughts.\nCould we schedule a meeting to discuss my feedback in detail?"
-			},
-			{
-				name: 'David Lee',
-				email: 'davidlee@example.com',
-				subject: 'New Project Idea',
-				date: '1 week ago',
-				teaser:
-					"I've been brainstorming and came up with an interesting project concept.\nDo you have time this week to discuss its potential impact and feasibility?"
-			},
-			{
-				name: 'Olivia Wilson',
-				email: 'oliviawilson@example.com',
-				subject: 'Vacation Plans',
-				date: '1 week ago',
-				teaser:
-					"Just a heads up that I'll be taking a two-week vacation next month.\nI'll make sure all my projects are up to date before I leave."
-			},
-			{
-				name: 'James Martin',
-				email: 'jamesmartin@example.com',
-				subject: 'Re: Conference Registration',
-				date: '1 week ago',
-				teaser:
-					"I've completed the registration for the upcoming tech conference.\nLet me know if you need any additional information from my end."
-			},
-			{
-				name: 'Sophia White',
-				email: 'sophiawhite@example.com',
-				subject: 'Team Dinner',
-				date: '1 week ago',
-				teaser:
-					"To celebrate our recent project success, I'd like to organize a team dinner.\nAre you available next Friday evening? Please let me know your preferences."
-			}
-		]
-	};
+
 
 	let { ref = $bindable(null), ...restProps }: ComponentProps<typeof Sidebar.Root> = $props();
-
-	let activeItem = $state(data.navMain[0]);
-	let mails = $state(data.mails);
-	const sidebar = useSidebar();
 
 	// State for create new file/folder
 	let showCreateInput = $state(false);
@@ -288,7 +125,7 @@
 				<Sidebar.MenuItem>
 					<Sidebar.MenuButton size="lg" class="md:h-8 md:p-0">
 						{#snippet child({ props })}
-							<a href="##" {...props}>
+							<a href="/" {...props}>
 								<div
 									class="flex aspect-square size-8 items-center justify-center rounded-lg bg-sidebar-primary text-sidebar-primary-foreground"
 								>
@@ -307,29 +144,6 @@
 			<Sidebar.Group>
 				<Sidebar.GroupContent class="px-1.5 md:px-0">
 					<Sidebar.Menu>
-						{#each data.navMain as item (item.title)}
-							<Sidebar.MenuItem>
-								<Sidebar.MenuButton
-									tooltipContentProps={{
-										hidden: false
-									}}
-									onclick={() => {
-										activeItem = item;
-										const mail = data.mails.sort(() => Math.random() - 0.5);
-										mails = mail.slice(0, Math.max(5, Math.floor(Math.random() * 10) + 1));
-										sidebar.setOpen(true);
-									}}
-									isActive={activeItem.title === item.title}
-									class="px-2.5 md:px-2"
-								>
-									{#snippet tooltipContent()}
-										{item.title}
-									{/snippet}
-									<item.icon />
-									<span>{item.title}</span>
-								</Sidebar.MenuButton>
-							</Sidebar.MenuItem>
-						{/each}
 						<Sidebar.MenuItem>
 							<Sidebar.MenuButton
 								tooltipContentProps={{
