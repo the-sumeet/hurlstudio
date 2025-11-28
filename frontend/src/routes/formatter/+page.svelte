b5056ade32c2ab1f801241a9839684f5630e5dbd
sumeet <Sumeet.Mathpati@steel-eye.com>
2025-11-29 04:43:09 +0530

    exit shortcut

--- frontend/src/routes/formatter/+page.svelte
+++ frontend/src/routes/formatter/+page.svelte
@@ -3,13 +3,23 @@
 	import { Button } from '$lib/components/ui/button/index.js';
 	import * as NativeSelect from '$lib/components/ui/native-select/index.js';
 	import MonacoEditor from '$lib/components/MonacoEditor.svelte';
+	import { goto } from '$app/navigation';
+	import { Kbd } from '$lib/components/ui/kbd/index.js';
 
 	let value = '';
 
 	function onchange(newContent: string) {
 		value = newContent;
 	}
+
+	function handleKeydown(event: KeyboardEvent) {
+		if (event.key === 'Escape') {
+			goto('/');
+		}
+	}
 </script>
+
+<svelte:window onkeydown={handleKeydown} />
 
 <Card.Root class="h-full rounded-none">
 	<Card.Header>
@@ -25,6 +35,6 @@
 		<MonacoEditor {onchange} />
 	</Card.Content>
 	<Card.Footer class="flex gap-2">
-		<Button href="/" variant="outline">Close</Button>
+		<Button href="/" variant="outline" class="gap-2">Close <Kbd>ESC</Kbd></Button>
 	</Card.Footer>
 </Card.Root>
